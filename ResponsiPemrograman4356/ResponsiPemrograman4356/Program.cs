56a9a6c291ad55d31d699cd942f38cab3e6a141e
jessicavg <jessica.vegaa@students.amikom.ac.id>
2022-04-19 11:58:29 +0700

    Selesai

---
+++ ResponsiPemrograman4356/ResponsiPemrograman4356/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman4356
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nama     : Jessica Vega Nainggolan");
+            Console.WriteLine("NIM      : 21.11.4356");
+            Console.WriteLine("Kelas    : 21-IF-08");
+
+            Console.WriteLine("\n");
+
+
+            Karyawan karyawan1 = new Karyawan(190302123, "Paijo", 3000000);
+            Karyawan karyawan2 = new Karyawan(190302124, "Jono", 2000000);
+            Karyawan karyawan3 = new Karyawan(190302125, "Tono", -1000000);
+
+
+            Console.WriteLine("--------------------------------------------------");
+            Console.WriteLine(" NO | NIK / NAMA\t\t | Gaji Bulanan\t |");
+            Console.WriteLine("--------------------------------------------------");
+            Console.WriteLine(" 1. | {0} {1} \t\t | {2}\t |", karyawan1.NIK, karyawan1.Nama, karyawan1.GajiBulanan);
+            Console.WriteLine(" 2. | {0} {1} \t\t | {2}\t |", karyawan2.NIK, karyawan2.Nama, karyawan2.GajiBulanan);
+            Console.WriteLine(" 3. | {0} {1} \t\t | {2}\t\t |", karyawan3.NIK, karyawan3.Nama, karyawan3.GajiBulanan);
+
+            Console.WriteLine("\n\nAsyikkkkk kenaikan gaji 10%!!\n");
+
+            Console.WriteLine("--------------------------------------------------");
+            Console.WriteLine(" NO | NIK / NAMA\t\t | Gaji Bulanan\t |");
+            Console.WriteLine("--------------------------------------------------");
+            Console.WriteLine(" 1. | {0} {1} \t\t | {2}\t |", karyawan1.NIK, karyawan1.Nama, karyawan1.NaikGaji);
+            Console.WriteLine(" 2. | {0} {1} \t\t | {2}\t |", karyawan2.NIK, karyawan2.Nama, karyawan2.NaikGaji);
+            Console.WriteLine(" 3. | {0} {1} \t\t | {2}\t\t |", karyawan3.NIK, karyawan3.Nama, karyawan3.GajiBulanan);
+
+
+            Console.ReadKey();
+        }
+    }
+}
